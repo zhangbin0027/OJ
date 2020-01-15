3071d1678169fb9e82d4b91e56d99ee174940a72
zhangbin0027 <zhangbin0027@github.com>
2020-01-15 11:40:07 +0800

    141.环形链表(set集合法)

---
+++ linkedList/141.py
@@ -0,0 +1,19 @@
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution(object):
+    def hasCycle(self, head):
+        """
+        :type head: ListNode
+        :rtype: bool
+        """
+        s = set()
+        while head:
+            if head in s:
+                return True
+            s.add(head)
+            head = head.next
+        return False
