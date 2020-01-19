2f7170a10cdc6669d9dc66f9e6fc973c98c755ce
zhangbin0027 <zhangbin0027@github.com>
2020-01-19 18:07:05 +0800

    144.二叉树的前序遍历(栈)

---
+++ LinkList_Program/144.py
@@ -0,0 +1,28 @@
+# Definition for a binary tree node.
+class TreeNode(object):
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+class Solution(object):
+    # 栈方法实现二叉树的前序遍历
+    def preorderTraversal(self, root):
+        """
+        :type root: TreeNode
+        :rtype: List[int]
+        """
+        if root is None:
+            return []
+        stack, output = [root, ], []
+        
+        while stack:
+            root = stack.pop()
+            if root is not None:
+                output.append(root.val)
+                if root.right is not None:
+                    stack.append(root.right)
+                if root.left is not None:
+                    stack.append(root.left)
+
+        return output
